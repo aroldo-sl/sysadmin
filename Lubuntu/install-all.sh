20cb7aea9ad39f38361986ad69c5f17aeb8cca63
aroldo <asouzaleite@gmx.de>
2019-08-20 18:25:56 +0200

    hopefully now clean package configuration.

--- Lubuntu/install-all.sh
+++ Lubuntu/install-all.sh
@@ -24,38 +24,6 @@ aptitude install -y xsel
 ###### FAILED
 ##### aptitude install -y alsamixer
 
-# ############################# Python-3.6
-# aptitude install -y    libssl-dev 
-# aptitude install -y    zlib1g-dev 
-# aptitude install -y    libbz2-dev 
-# aptitude install -y    libreadline-dev 
-# aptitude install -y    libsqlite3-dev 
-# aptitude install -y    libncurses5-dev 
-# aptitude install -y    libncursesw5-dev 
-# aptitude install -y    xz-utils 
-# aptitude install -y    tk-dev 
-# aptitude install -y    libffi-dev 
-# aptitude install -y    liblzma-dev 
-# aptitude install -y    python3-dev 
-# aptitude install -y    python-six
-# aptitude install -y    python3.6-dev    
-# aptitude install -y    libpython3.6
-# aptitude install -y    libpython3.6-dbg
-# aptitude install -y    libpython3.6-dev
-# aptitude install -y    libxml2-dev
-# aptitude install -y    libxslt-dev
-# aptitude install -y    pandoc
-#################### FAILED
-###### aptitude install -y pil
-# ######## for sip (PyQt5)
-# aptitude install -y flex
-# aptitude install -y bison
-# ####################### https://askubuntu.com/questions/612314/how-to-install-pyqt-for-python-3-in-ubuntu-14-10
-# aptitude install -y qt5-default
-# aptitude install -y python3-pyqt5
-# aptitude install -y pyqt5-dev-tools
-# aptitude install -y qttools5-dev-tools
-# ############ usage: qtchooser -run-tool=designer -qt=5
 
 # ################# Epson L555
 # apt-get -f install
@@ -64,11 +32,6 @@ aptitude install -y xsel
 # dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
 # aptitude install printer-driver-escpr
 
-# ########################## PyInstaller
-
-# # apt-add-repository  http://ftp.linux-foundation.org/pub/lsb/repositories/debian lsb-4.0 main
-# # aptitude update
-# aptitude install lsb lsb-build-cc
 
 
 ###########################
--- Lubuntu/prepare-for-Python3_7.sh
+++ Lubuntu/prepare-for-Python3_7.sh
@@ -2,31 +2,40 @@
 
 echo "Preparing the system for compiling Python3.7 without sudo rights."
 
-# aptitude install -y    build-essential 
-# aptitude install -y    libssl-dev 
-# aptitude install -y    zlib1g-dev 
-# aptitude install -y    libbz2-dev 
-# aptitude install -y    libreadline-dev 
-# aptitude install -y    libsqlite3-dev 
-# aptitude install -y    wget 
-# aptitude install -y    make 
-# aptitude install -y    curl 
-# aptitude install -y    llvm 
-# aptitude install -y    libncurses5-dev 
-# aptitude install -y    libncursesw5-dev 
-# aptitude install -y    xz-utils 
-# aptitude install -y    tk-dev 
-# aptitude install -y    libffi-dev 
-# aptitude install -y    liblzma-dev 
-# aptitude install -y    python3-dev 
-# aptitude install -y    python-six
-# aptitude install -y    python3.6-dev    
-# aptitude install -y    libpython3.6
-# aptitude install -y    libpython3.6-dbg
-# aptitude install -y    libpython3.6-dev
-# aptitude install -y    libxml2-dev
-# aptitude install -y    libxslt-dev
-apt-get build-dep python3-lxml
+aptitude install -y    build-essential 
+aptitude install -y    libssl-dev 
+aptitude install -y    zlib1g-dev 
+aptitude install -y    libbz2-dev 
+aptitude install -y    libreadline-dev 
+aptitude install -y    libsqlite3-dev 
+aptitude install -y    wget 
+aptitude install -y    make 
+aptitude install -y    curl 
+aptitude install -y    llvm 
+aptitude install -y    libncurses5-dev 
+aptitude install -y    libncursesw5-dev 
+aptitude install -y    xz-utils 
+aptitude install -y    tk-dev 
+aptitude install -y    libffi-dev 
+aptitude install -y    liblzma-dev 
+aptitude install -y    python3-dev 
+aptitude install -y    python-six
+aptitude install -y    python3.6-dev    
+aptitude install -y    libpython3.6
+aptitude install -y    libpython3.6-dbg
+aptitude install -y    libpython3.6-dev
+aptitude install -y    libxml2-dev
+aptitude install -y    libxslt-dev
+# ########################## PyInstaller
+
+apt-add-repository  http://ftp.linux-foundation.org/pub/lsb/repositories/debian lsb-4.0 main
+aptitude update
+aptitude install lsb lsb-build-cc
+
+
+###### FAILED:
+#### add-apt-repository "http://deb.debian.org/debian stretch main contrib non-free"
+#### apt-get build-dep python3-lxml
 
 ######### for sip (PyQt5)
 # aptitude install -y flex
