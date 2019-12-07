4e6d59c1a3ce4721b1a898abd439c8d0c5a79e31
aroldo <asouzaleite@gmx.de>
2019-12-07 14:36:51 +0100

    installed musescore3

--- Lubuntu/install-all.sh
+++ Lubuntu/install-all.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 echo "Installing basic packages in Lubuntu 19.04"
-apt-get  install -y aptitude
-aptitude install -y build-essential
-aptitude install -y make 
-aptitude install -y llvm 
-aptitude install -y emacs
-aptitude install -y most
-aptitude install -y wget 
-aptitude install -y curl 
-aptitude install -y gnome-system-tools
-aptitude install -y thunderbird
-aptitude install -y chromium-browser
-aptitude install -y git
-aptitude install -y libjpeg8-dev
-aptitude install -y zlib1g-dev
-aptitude install -y dvdbackup
-aptitude install -y libdvdcss2
-aptitude install -y dvdauthor
-aptitude install -y libdvdcss-dev
-aptitude install -y xsel
+# apt-get  install -y aptitude
+# aptitude install -y build-essential
+# aptitude install -y make 
+# aptitude install -y llvm 
+# aptitude install -y emacs
+# aptitude install -y most
+# aptitude install -y wget 
+# aptitude install -y curl 
+# aptitude install -y gnome-system-tools
+# aptitude install -y thunderbird
+# aptitude install -y chromium-browser
+# aptitude install -y git
+# aptitude install -y libjpeg8-dev
+# aptitude install -y zlib1g-dev
+# aptitude install -y dvdbackup
+# aptitude install -y libdvdcss2
+# aptitude install -y dvdauthor
+# aptitude install -y libdvdcss-dev
+# aptitude install -y xsel
+
+# aptitude install -y fonts-nanum
 
 ###### FAILED
 ##### aptitude install -y alsamixer
@@ -32,6 +34,9 @@ aptitude install -y xsel
 # dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
 # aptitude install printer-driver-escpr
 
+
+# #####################
+aptitude install -y musescore3
 
 
 ###########################
