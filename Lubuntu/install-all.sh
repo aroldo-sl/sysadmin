019787e8b08eb89518feffc37c76d5e6fb4debed
aroldo <asouzaleite@gmx.de>
2020-05-22 09:20:17 +0200

    install-all.sh runs without stop. No errors. Terminator is missing.

--- Lubuntu/install-all.sh
+++ Lubuntu/install-all.sh
@@ -2,44 +2,51 @@
 #!/bin/bash
 
 echo "Installing basic packages in Lubuntu 19.10"
-# apt-get  install -y aptitude
-# aptitude install -y build-essential
-# aptitude install -y make 
-# aptitude install -y llvm 
-# aptitude install -y emacs
-# aptitude install -y most
-# aptitude install -y wget 
-# aptitude install -y curl 
-# aptitude install -y gnome-system-tools
-# aptitude install -y thunderbird
-# aptitude install -y chromium-browser
-# aptitude install -y git
-# aptitude install -y libjpeg8-dev
-# aptitude install -y zlib1g-dev
-# aptitude install -y dvdbackup
-# aptitude install -y libdvdcss2
-# aptitude install -y dvdauthor
-# aptitude install -y libdvdcss-dev
-# aptitude install -y xsel
-# aptitude install -y musescore3
-# aptitude install -y mumble
-# aptitude install -y fonts-nanum
-# aptitude install -y pandoc
-# apt-add-repository ppa:gnome-terminator
-# aptitude -y install terminator
-# aptitude -y install fd-find
-# aptitude -y install locate
-# aptitude -y install gedit
-# aptitude -y install qalculate
-# aptitude install -y software-properties conmon
-# add-apt-repository -y ppa:projectatomic/ppa
-aptitude install -y podman
+apt-get  install -y aptitude
+aptitude install -y build-essential
+aptitude install -y make 
+aptitude install -y llvm 
+aptitude install -y emacs
+aptitude install -y most
+aptitude install -y wget 
+aptitude install -y curl 
+aptitude install -y gnome-system-tools
+aptitude install -y thunderbird
+aptitude install -y chromium-browser
+aptitude install -y git
+aptitude install -y libjpeg8-dev
+aptitude install -y zlib1g-dev
+aptitude install -y dvdbackup
+aptitude install -y libdvdcss2
+aptitude install -y dvdauthor
+aptitude install -y libdvdcss-dev
+aptitude install -y xsel
+aptitude install -y musescore3
+aptitude install -y mumble
+aptitude install -y fonts-nanum
+aptitude install -y pandoc
+aptitude -y install fd-find
+aptitude -y install locate
+aptitude -y install gedit
+aptitude -y install qalculate
+aptitude install -y gparted
 # ################# Epson L555
 # apt-get -f install
 # aptitude install -y lsb-core
 # aptitude install -y lsb-printing
 # dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
-# aptitude install printer-driver-escpr
+# aptitude install -y printer-driver-escpr
+
+#######################
+# add-apt-repository -y ppa:projectatomic/ppa
+# apt-add-repository ppa:gnome-terminator
+# aptitude -y install terminator
+
+#####################
+
+# aptitude install -y software-properties conmon
+#####
+# aptitude install -y podman
 
 ###### FAILED
 ##### aptitude install -y alsamixer
