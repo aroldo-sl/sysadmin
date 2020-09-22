dd74d8cdc4b9110a7ff21c37421daa617bfae43f
aroldo <asouzaleite@gmx.de>
2020-09-22 22:54:10 +0200

    Nach Mint-Installation.

--- Lubuntu/install-all.sh
+++ Lubuntu/install-all.sh
@@ -2,52 +2,48 @@
 #!/bin/bash
 
 echo "Installing basic packages in Lubuntu 19.10"
-# apt-get  install -y aptitude
-# aptitude install -y build-essential
-# aptitude install -y make 
-# aptitude install -y llvm 
-# aptitude install -y emacs
-# aptitude install -y most
-# aptitude install -y wget 
-# aptitude install -y curl 
-# aptitude install -y gnome-system-tools
-# aptitude install -y thunderbird
-# aptitude install -y chromium-browser
-# aptitude install -y git
-# aptitude install -y libjpeg8-dev
-# aptitude install -y zlib1g-dev
-# aptitude install -y dvdbackup
-# aptitude install -y libdvdcss2
-# aptitude install -y dvdauthor
-# aptitude install -y libdvdcss-dev
-# aptitude install -y xsel
-# aptitude install -y musescore3
-# aptitude install -y mumble
-# aptitude install -y fonts-nanum
-# aptitude install -y pandoc
-# aptitude -y install fd-find
-# aptitude -y install locate
-# aptitude -y install gedit
-# aptitude -y install qalculate
-# aptitude install -y gparted
-# aptitude -y install blueman
-################# Epson L555
-# apt-get -f install
-# aptitude install -y lsb-core
-# aptitude install -y lsb-printing
-# dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
-# aptitude install -y printer-driver-escpr
+apt-get  install -y aptitude
+aptitude install -y build-essential
+aptitude install -y make 
+aptitude install -y llvm 
+aptitude install -y emacs
+aptitude install -y most
+aptitude install -y wget 
+aptitude install -y curl 
+aptitude install -y gnome-system-tools
+aptitude install -y thunderbird
+aptitude install -y chromium-browser
+aptitude install -y git
+aptitude install -y libjpeg8-dev
+aptitude install -y zlib1g-dev
+aptitude install -y dvdbackup
+aptitude install -y libdvdcss2
+aptitude install -y dvdauthor
+aptitude install -y libdvdcss-dev
+aptitude install -y xsel
+aptitude install -y musescore3
+aptitude install -y mumble
+aptitude install -y fonts-nanum
+aptitude install -y pandoc
+aptitude -y install fd-find
+aptitude -y install locate
+aptitude -y install gedit
+aptitude -y install qalculate
+aptitude install -y gparted
+aptitude -y install blueman
+Epson L555
+apt-get -f install
+aptitude install -y lsb-core
+aptitude install -y lsb-printing
+dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
+aptitude install -y printer-driver-escpr
 
+add-apt-repository -y ppa:projectatomic/ppa
+apt-add-repository ppa:gnome-terminator
+aptitude -y install terminator
 
-# add-apt-repository -y ppa:projectatomic/ppa
-# apt-add-repository ppa:gnome-terminator
-# aptitude -y install terminator
-
-
-
-# aptitude install -y software-properties conmon
-
-# aptitude install -y podman
+aptitude install -y software-properties conmon
+aptitude install -y podman
 
 ################# <fsarchiver> ##################
 aptitude -y  install libzip-dev
@@ -78,11 +74,6 @@ aptitude -y install fsarchiver
 
 ################ </fsarchiver> #################
 
-# ###### FAILED
-# ##### aptitude install -y alsamixer
-# add-apt-repository ppa:mkusb/ppa
-# aptitude -y update
-# apt install --install-recommends mkusb mkusb-nox usb-pack-efi
 
 
 
