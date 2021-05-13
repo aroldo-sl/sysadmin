f8c9b1818f115120f4d6a02ca30f6920d71e6c31
aroldo-sl <aroldo-sl@localhost>
2021-05-13 22:05:23 +0200

    purged small bash bug

--- LinuxMint-20.04/usr/local/sbin/install-00.sh
+++ LinuxMint-20.04/usr/local/sbin/install-00.sh
@@ -3,9 +3,13 @@
 # @date: 2021-05-13
 # @modified: 2021-05-13
 # @comment: Calls install_00.py 
-logfile="install.log"
-echo $("BEGIN of $0")
+this_script=$(realpath $0)
+workdir=$(dirname $this_script)
+cd $workdir
+echo "work directory:$workdir)"
+logfile="$workdir/install.log"
+echo "BEGIN of $this_script"
 echo $(date +"%FT%H-%M-%S") |tee -a $logfile
 time ./install_00.py |tee -a $logfile
-echo "END of $0"
+echo "END of $this_script"
 echo "logfile: $(realpath $logfile)"
