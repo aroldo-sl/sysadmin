5170ac26fc1da447ab24dc81f9364a52aa43e160
aroldo-sl <aroldo-sl@localhost>
2021-06-11 16:48:27 +0200

    Sanitizing trash.py

--- LinuxMint-20.04/usr/local/bin/trash.py
+++ LinuxMint-20.04/usr/local/bin/trash.py
@@ -9,28 +9,42 @@ Renames filename to filename.Trash
 import sys
 import os
 
+def trash_one(filename, trash_ext = ".Trash"):
+    """
+    Trashes one file or directory.
+    """
+    if not os.path.exists(filename):
+        print("not found:", filename)
+        trashed_item = (filename, None)
+        return trashed_item
+    filename = os.path.normpath(filename)
+    filename_Trash = filename + trash_ext
+    if os.path.exists(filename_Trash):
+        trash_one(filename_Trash, trash_ext)
+    os.rename(filename, filename_Trash)
+    trashed_item = (filename, filename_Trash)
+    return trashed_item
+
+
 def trash_all(filenames = (), trash_ext=".Trash"):
     """
     Trashes every file in the list.
     """
+    trashed_items = []
     for filename in filenames:
-        if not os.path.exists(filename):
-            print("not found:", filename)
-            continue
-        filename = os.path.normpath(filename)
-        filename_Trash = filename + trash_ext
-        if os.path.exists(filename_Trash):
-            print("{filename} already trashed.".format(filename=filename) )
-            continue
-        os.rename(filename, filename + trash_ext)
-        print('trashed:', filename, file=sys.stderr)
-    return filenames
+        trashed_item = trash_one(filename, trash_ext = trash_ext)
+        trashed_items.append(trashed_item)
+    return trashed_items
 
 def _script():
        '''
        Invoked only if this module runs as the main script.
        '''
-       trash_all(sys.argv[1:])
+       trashed_items = trash_all(sys.argv[1:])
+       for filename, filename_Trash in trashed_items:
+           msg= "{filename} -> {filename_Trash}".format(
+               filename = filename, filename_Trash = filename_Trash)
+           print (msg)
  
 if __name__=='__main__':
    _script()
