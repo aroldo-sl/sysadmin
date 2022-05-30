8f0282ee62dcf9875b98af2206b45b3a8efc3aca
aroldo <asouzaleite@gmx.de>
2022-05-30 10:55:28 +0200

    now versioning install-utils

---
+++ LM/install-utils/00/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# @file: install-00.sh
+# @date: 2021-05-13
+# @modified: 2021-05-13
+# @comment: Calls install_00.py 
+this_script=$(realpath $0)
+workdir=$(dirname $this_script)
+cd $workdir
+echo "work directory:$workdir"
+logfile="${this_script}.log"
+echo "BEGIN of $this_script"
+echo $(date +"%FT%H-%M-%S") |tee -a $logfile
+cmd="./install.py $@ |tee -a $logfile"
+echo $cmd
+# eval $cmd
+echo "END of $this_script"
+echo "logfile: $(realpath $logfile)"
---
+++ LM/install-utils/emacs/install-emacs-dep.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+sudo apt-get install autoconf \
+                     automake \
+                     libtool \
+                     texinfo \
+                     build-essential \
+                     xorg-dev \
+                     libgtk-3-dev \
+                     libjpeg-dev \
+                     libncurses5-dev \
+                     libdbus-1-dev \
+                     libgif-dev \
+                     libtiff-dev \
+                     libm17n-dev \
+                     libpng-dev \
+                     librsvg2-dev \
+                     libotf-dev \
+                     libgnutls28-dev \
+                     libxml2-dev \
+                     libxpm-dev
