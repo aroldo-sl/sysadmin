940885663d025e80458d96a399d6c2e43e379af2
aroldo <asouzaleite@gmx.de>
2023-08-26 23:20:21 +0200

    Must be testet.

--- misc-utils/mount-partlabel.sh
+++ misc-utils/mount-partlabel.sh
@@ -7,24 +7,42 @@ error_log=$(mktemp)
 error_handle() {
     error_code=$?
     printf "ERROR in $script:${BASH_LINENO[0]}\n"
-    printf "$BASH_COMMAND\n"
+    printf "Bash command:$BASH_COMMAND\n"
+    cat $error_log
+    printf "error code:$error_code\n"
+    error_log=$(mktemp)
+}
+trap error_handle ERR
+
+exit_handle() {
+    error_code=$?
+    printf "ERROR (EXIT) in $script:${BASH_LINENO[0]}\n"
+    printf "Bash command:$BASH_COMMAND\n"
     cat $error_log
     printf "error code:$error_code\n"
     exit $error_code
 }
-trap error_handle ERR
-# trap error_handle EXIT
+trap exit_handle EXIT
+
+
 partlabel=$1
 device=/dev/disk/by-partlabel/$partlabel
 ls $device 2>$error_log
-# mount_dir=/mnt/$partlabel
-# if [[ ! -d $mount_dir ]]
-# then
-#       mkdir $mount_dir
-#       printf "created $mount_dir\n"
-# else
-#       printf "$mount_dir exists already\n"
-# fi
-# printf  "mount $device $mount_dir\n"
+mount_dir=/mnt/$partlabel
+if [[ ! -d $mount_dir ]]
+then
+      mkdir $mount_dir
+      printf "created $mount_dir\n"
+else
+      printf "$mount_dir already exists\n"
+fi
+if [[ ! $(mountpoint $mount_dir) ]]
+then
+       echo "$mount_dir is already a mount point"
+else
+       echo "mounting $device on $mount_dir"
+       mount $device $mount_dir
+fi
+echo "final mount status: $(findmnt -M $mount_dir -n -o  TARGET,SOURCE)"
 echo "End of $script"
 
