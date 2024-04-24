236f9001dcc0b62ef4bac0a0bc0266f964e856a6
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-04-24 12:33:44 +0200

    Beginning the cleanup

--- restic-devel/restic-snapshots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## file restic-snapshots.sh
-
-now="$(date +'%F-%H_%M_%S')"
-logfile="restic-snapshots.log"
-echo "repository:$(pwd)"  > "$logfile"
-echo "datetime:$now" >> "$logfile"
-echo "logging to $logfile"
-restic snapshots -vr "$(pwd)" | tee -a "$logfile"
