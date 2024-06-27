cd7debf912c5ade58d166ec3f821fbc80be202e6
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-06-27 07:34:11 +0200

    relevant install utils files now in root/.local/bin/install.d

--- LM/install-utils/wine/install-wine.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "installing wine"
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-apt-key add winehq.key
-apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-apt update
-apt install --install-recommends winehq-stable
