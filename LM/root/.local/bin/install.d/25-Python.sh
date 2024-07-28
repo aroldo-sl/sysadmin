69700739eb95942a4ad56067410f4dad3d982290
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-07-28 12:54:39 +0200

    GAVE UP rootless compiling Python from source; using pyenv instead.

--- LM/root/.local/bin/install.d/25-Python.sh
+++ LM/root/.local/bin/install.d/25-Python.sh
@@ -3,36 +3,47 @@
 ##  ##https://doc.pypy.org/en/latest/build.html
 ##  ##https://solarianprogrammer.com/2017/06/30/building-python-ubuntu-wsl-debian/
 packages=(
-build-essential 
-gcc 
-gdb 
-lcov 
-libbz2-dev
-libdb5.3-dev
-libexpat1-dev
-libffi-dev
-libgc-dev 
-libgdbm-compat-dev 
-libgdbm-dev
-liblzma-dev
-libmpdec-dev
-libncurses5-dev
-libncursesw5-dev
-libreadline-dev
-libreadline6-dev 
-libsqlite3-dev
-libssl-dev
-lzma 
-lzma-dev 
-make 
-pkg-config 
-python-cffi 
-python3-dev
-python3-tk
-python5-venv
-tk-dev 
-uuid-dev
-zlib1g-dev
+    build-essential
+    buip2
+    bzip2-devel
+    gcc 
+    gdb 
+    lcov 
+    libbz2-dev
+    libdb5.3-dev
+#
+    libexpat1-dev
+    libffi-dev
+    libffi-devel
+    libgc-dev 
+    libgdbm-compat-dev 
+    libgdbm-dev
+    libmpdec-dev
+    libncurses5-dev
+    libncursesw5-dev
+    libreadline-dev
+    libreadline6-dev 
+    libsqlite3-dev
+    libssl-dev
+    lzma 
+    lzma-dev 
+    make
+    openssl-devel
+    patch
+    pkg-config 
+    python-cffi 
+    python3-dev
+    python3-tk
+    python5-venv
+    readline
+    readline-devel
+    sqlite
+    sqlite-dev
+    tk-dev
+    tk-devel
+    uuid-dev
+    xz-devel
+    zlib1g-dev
 )
 
 for package in ${packages[*]}
