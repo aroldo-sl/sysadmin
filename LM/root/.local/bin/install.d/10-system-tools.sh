569a1ce5c15a38b1acf9d5476c942ed8a044f990
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-07-28 09:31:28 +0200

    Still trying to compile Python12

--- LM/root/.local/bin/install.d/10-system-tools.sh
+++ LM/root/.local/bin/install.d/10-system-tools.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 packages=(
-gnome-system-tools
-gparted
-restic
-rdiff-backup
-git
-uidmap
-podman
-skopeo
-openssh-client
-openssh-server
-git-extras
+#gnome-system-tools
+#gparted
+#restic
+#rdiff-backup
+#git
+#uidmap
+#podman
+#skopeo
+#openssh-client
+#openssh-server
+#git-extras
+stow
 )
 for package in ${packages[*]}
 do
--- LM/root/.local/bin/install.d/25-Python.sh
+++ LM/root/.local/bin/install.d/25-Python.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
+##  ##https://devguide.python.org/getting-started/setup-building/index.html#deps-on-linux
+##  ##https://doc.pypy.org/en/latest/build.html
+##  ##https://solarianprogrammer.com/2017/06/30/building-python-ubuntu-wsl-debian/
 packages=(
+build-essential 
+gcc 
+gdb 
+lcov 
+libbz2-dev
+libdb5.3-dev
+libexpat1-dev
+libffi-dev
+libgc-dev 
+libgdbm-compat-dev 
+libgdbm-dev
+liblzma-dev
+libmpdec-dev
+libncurses5-dev
+libncursesw5-dev
+libreadline-dev
+libreadline6-dev 
+libsqlite3-dev
+libssl-dev
+lzma 
+lzma-dev 
+make 
+pkg-config 
+python-cffi 
 python3-dev
 python3-tk
-python3-venv
+python5-venv
+tk-dev 
+uuid-dev
+zlib1g-dev
 )
 
 for package in ${packages[*]}
