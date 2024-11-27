06ad07fe6a5a86c98a0792022e3c84e4e5ad4b8e
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-11-27 11:32:53 +0100

    arp-scan for the rescue?

--- LM/root/.local/bin/install.d/10-utils.sh
+++ LM/root/.local/bin/install.d/10-utils.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 packages=(
-#vimtutor # not found
-#xapp #dependency error
-ffmpeg
-geany
-git
-git-extras
-gnome-system-tools
-gparted
-librsvg2-bin
-members
-most
-openssh-client
-openssh-server
-pandoc
-podman
-qtcreator
-rdiff-backup
-restic
-skopeo
-stow
-terminator
-tree
-uidmap
+        #vimtutor # not found
+        #xapp #dependency error
+        arp-scan
+        ffmpeg
+        geany
+        git
+        git-extras
+        gnome-system-tools
+        gparted
+        librsvg2-bin
+        members
+        most
+        nmap
+        openssh-client
+        openssh-server
+        pandoc
+        podman
+        qtcreator
+        rdiff-backup
+        restic
+        skopeo
+        stow
+        terminator
+        tree
+        uidmap
 )
 for package in ${packages[*]}
 do
