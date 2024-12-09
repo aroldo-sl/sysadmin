17b3f1b6b4e1b2fa60b11a8893c205abd7b0b9e9
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-12-09 13:59:41 +0100

    Now all ppa installation in 40-ppa-utils.sh

--- LM/root/.local/bin/install.d/40-ppa-utils.sh
+++ LM/root/.local/bin/install.d/40-ppa-utils.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 ## https://operavps.com/docs/fix-ubuntu-boot-problem/
+## <add repositories>
+add-apt-repository universe
 add-apt-repository ppa:yannubuntu/boot-repair
+add-apt-repository ppa:mkusb/ppa
 apt update
+## <add repositories>
+## <packages>
 apt install -y boot-repair
+apt install -y mkusb
+## <packages>
