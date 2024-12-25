4d35f2024ef7ca0eb94f328d1e5441d05cf215a6
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-12-25 11:58:44 +0100

    Installed gimp.

--- LM/root/.local/bin/install.d/10-utils.sh
+++ LM/root/.local/bin/install.d/10-utils.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 packages=(
-        arp-scan
-        default-jdk
-        ffmpeg
-        fsarchiver
-        geany
-        git
-        git-extras
-        gnome-system-tools
-        gparted
-        librsvg2-bin
-        members                   # bluetooth
-        most
-        nmap
-        openssh-client
-        openssh-server
-        pandoc
-        podman
-        qtcreator
-        rdiff-backup
-        restic
-        sshfs
-        skopeo
-        sshfs
-        stow
-        terminator
-        tree
-        uidmap
-        wl-clipboard
-        xclip
-        xsel
+        # arp-scan
+        # default-jdk
+        # ffmpeg
+        # fsarchiver
+        # geany
+          gimp
+        # git
+        # git-extras
+        # gnome-system-tools
+        # gparted
+        # librsvg2-bin
+        # members                   # bluetooth
+        # most
+        # nmap
+        # openssh-client
+        # openssh-server
+        # pandoc
+        # podman
+        # qtcreator
+        # rdiff-backup
+        # restic
+        # sshfs
+        # skopeo
+        # sshfs
+        # stow
+        # terminator
+        # tree
+        # uidmap
+        # wl-clipboard
+        # xclip
+        # xsel
 )
 for package in ${packages[*]}
 do
