06cafcac8f370934f5a62b57d2ca38084727f8d4
Aroldo Souza-Leite <asouzaleite@gmx.de>
2025-06-29 13:17:35 +0200

    install.py should have no dependencies.

---
+++ LM/install.d
@@ -0,0 +1,1 @@
+root/.local/bin/install.d/
---
+++ LM/root/.local/bin/install.d/install.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Installs packages from 'packages.toml'.
+"""
+from tomllib import load
+from pprint import pformat
+
+# # #  [logging]
+def _make_slog():
+    """
+    Diese Funktion benutzt nur die Standardbibliothek.
+
+    Da diese Funktion nur einen ad hoc Logger erstellt,
+    importier sie innerhalb der Funktion, sehr untypischerweise.
+    Normalerweise sollten die imports modulweit gelten.
+    """
+    import sys   # non-module-global import: bad Python style!
+    import time
+    import logging
+    logging.basicConfig(
+        format = ("\n%(levelname)s:[%(name)s][%(asctime)s]"
+                  "[%(module)s.%(funcName)s]\n%(message)1s"),
+        stream = sys.stderr)
+    _slog = logging.getLogger(__name__ + str(time.time()).replace('.','_'))
+    _slog.setLevel(logging.DEBUG)
+    return _slog
+
+_slog = _make_slog() # _slog ist Modulweit sichtbar.
+
+
+
+def retrieve_data(toml_filename = 'packages.toml'):
+    """
+    Retrieves the data from the TOML file.
+    """
+    with open(toml_filename, "rb") as toml_file:
+        data = load(toml_file)
+    _slog.debug("TOML data.")
+    return data
+
+def display_data(toml_filename = 'packages.toml'):
+    """
+    Displays the TOML data.
+    """
+    data = retrieve_data(toml_filename)
+    pretty_data = pformat(data)
+    return pretty_data
+    
+
+
+
+
+
