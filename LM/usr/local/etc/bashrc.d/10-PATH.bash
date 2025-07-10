118f1b6b4479afd7efe84a15c5576ff7bf4c49d8
Aroldo Souza-Leite <asouzaleite@gmx.de>
2025-07-10 10:08:33 +0200

    Setting the bash is working well.

--- LM/usr/local/etc/bashrc.d/10-PATH.bash
+++ LM/usr/local/etc/bashrc.d/10-PATH.bash
@@ -1,4 +1,11 @@
 echo  "beginning of 10-PATH.bash"
 mkdir -p "$HOME/.local/bin"
-export PATH="$HOME/.local/bin:$PATH"
+HOME_BIN=$HOME/.local/bin
+if ! [[ $PATH =~ :*${HOME_BIN} ]]
+then
+	export PATH==$HOME_BIN:$PATH
+	echo "$HOME_BIN added to PATH"
+else
+	echo "$HOME_BIN already in PATH"
+fi
 echo "end of 10-PATH.bash"
