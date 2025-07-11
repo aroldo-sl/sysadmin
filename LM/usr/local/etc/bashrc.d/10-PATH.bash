dd0e1275734f5efc0ca20a40f32ce602c347a87c
aroldo <asouzaleite@gmx.de>
2025-07-11 19:45:58 +0200

    Sanitizing.

--- LM/usr/local/etc/bashrc.d/10-PATH.bash
+++ LM/usr/local/etc/bashrc.d/10-PATH.bash
@@ -3,8 +3,5 @@ mkdir -p "$HOME_BIN"
 if ! [[ $PATH =~ ^:*${HOME_BIN} ]]
 then
 	export PATH="$HOME_BIN:$PATH"
-	echo "$HOME_BIN prepended to PATH"
-else
-	echo "$HOME_BIN already in the beginning of PATH"
 fi
 echo "PATH=$PATH"
